0889f96fbac24f8e96712023d56b698836c11aaa
Breno Teixeira <brenotx@gmail.com>
2017-10-16 10:13:25 -0200

    Fixing insert error

--- controllers/userNivelCtrl.js
+++ controllers/userNivelCtrl.js
@@ -1,24 +1,39 @@
-let ObjectId = require('mongoose').Types.ObjectId; 
+let ObjectId = require('mongoose').Types.ObjectId;
 
 const UserNivel = require('../models/userNivel');
 
 exports.setUserMaxNivel = function(req, res, next) {
     const user_id = req.body.user_id;
 
-    const obj = req.body;
-    delete obj._id;
+    // const obj = req.body;
+    // delete obj._id;
 
-    UserNivel.findOneAndUpdate({ user_id: user_id }, req.body, { upsert: true }, function(err, result){
+    UserNivel.findOne({ user_id: user_id }, function(err, userNivel) {
+        if (req.body.maxNivel > userNivel.maxNivel) {
+            userNivel.maxNivel = req.body.maxNivel;
+            userNivel.save(function(err) {
+                if (err) {
+                    return next(err);
+                }
+                return res.send('Succesfully updated');
+            });
+        } else {
+            return res.send('The user has a higher progress');
+        }
         if (err) return res.send(500, { error: err });
-        
-        return res.send("Succesfully saved");
     });
+
+    // UserNivel.findOneAndUpdate({ user_id: user_id }, req.body, { upsert: true }, function(err, result) {
+    //     if (err) return res.send(500, { error: err });
+
+    //     return res.send('Succesfully saved');
+    // });
 };
 
 exports.userMaxNivel = function(req, res, next) {
     const user_id = req.params.userId;
 
-    UserNivel.findOne({ 'user_id': new ObjectId(user_id)}, function (err, userMaxNivel) {
+    UserNivel.findOne({ user_id: new ObjectId(user_id) }, function(err, userMaxNivel) {
         res.send(userMaxNivel);
     });
 };
